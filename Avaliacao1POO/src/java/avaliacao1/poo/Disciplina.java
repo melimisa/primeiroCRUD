063a99a8817622e9d2c193672528dfd34a571f68
melimisa <isabela.pereiramelim@gmail.com>
2020-10-04 17:57:23 -0300

    Classe Disciplina.java criada
    Classe com construtor, getters e setters

---
+++ Avaliacao1POO/src/java/avaliacao1/poo/Disciplina.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avaliacao1.poo;
+
+/**
+ *
+ * @author isame
+ */
+public class Disciplina {
+    private String nome;
+    private String ementa;
+    private int ciclo;
+
+    public Disciplina(String nome, String ementa, int ciclo) {
+        this.nome = nome;
+        this.ementa = ementa;
+        this.ciclo = ciclo;
+    }
+    private float nota;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmenta() {
+        return ementa;
+    }
+
+    public void setEmenta(String ementa) {
+        this.ementa = ementa;
+    }
+
+    public int getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(int ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+    
+}
